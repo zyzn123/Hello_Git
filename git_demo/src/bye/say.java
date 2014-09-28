0dacf1b8ae085bb3531806e83dcd88f2d8d5da5e
xsvm <klxx@outlook.com>
2014-09-28 11:24:27 +0800

    this is the first revised edition

--- git_demo/src/bye/say.java
+++ git_demo/src/bye/say.java
@@ -7,7 +7,9 @@ public class say {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("how can i get the truth");
+		System.out.println(" how can i get the truth");
+		System.out.println(" maybe someone tells the lie");
+		System.out.println(" the truth or the fake ?");
 	}
 
 }
