7cfcd94d89c68f0374316867c7a3a1e53bad5db7
xsvm <klxx@outlook.com>
2014-09-28 11:16:12 +0800

    first commit for git_demo

---
+++ git_demo/src/bye/say.java
@@ -0,0 +1,13 @@
+package bye;
+
+public class say {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("how can i get the truth");
+	}
+
+}
